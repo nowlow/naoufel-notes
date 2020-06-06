b5ea41fb5b8f8da752b706dda584479cf5e63ab8
Naoufel Berrada <naoufel.berrada@epitech.eu>
2020-06-06 19:36:27 +0200

    added: note publishing

--- front/components/pages/NewNote.js
+++ front/components/pages/NewNote.js
@@ -1,10 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import Article from "../../components/pages/Article";
-import { ApolloClient, gql } from "apollo-boost";
-import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
-import config from "../../config";
+import React from "react"
+import styled from "styled-components"
+import { ApolloClient, gql } from "apollo-boost"
+import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory"
+import { HttpLink } from "apollo-link-http"
+import config from "../../config"
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -17,14 +16,124 @@ const client = new ApolloClient({
 });
 
 const Page_ = styled.section`
+    width: 100%;
+    height: 100%;
 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    form {
+        width: 40%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    form > * {
+        box-sizing: border-box;
+    }
+
+    form > *:not(:last-child) {
+        margin-bottom: 20px;
+    }
+
+    form input {
+        width: 100%;
+        border: 1px solid rgba(200, 200, 200, 1);;
+        padding: 10px;
+    }
+
+    form textarea {
+        width: 100%;
+        height: 250px;
+        border: 1px solid rgba(200, 200, 200, 1);
+        padding: 10px;
+        resize: none;
+    }
+
+    .submit {
+        width: 100%;
+        padding: 10px;
+        background-color: black;
+        color: white;
+        text-align: center;
+        font-weight: bold;
+    }
+
+    ${config.responsive.query} {
+        form {
+            width: 100%;
+            align-items: baseline;
+        }
+
+        form > *:not(:last-child) {
+            margin-bottom: 40px;
+        }
+
+        form h3 {
+            font-size: 4em;
+        }
+
+        form input, form textarea {
+            border: 2px solid rgb(200, 200, 200);
+            font-size: 2em;
+        }
+
+        form textarea {
+            height: 50vh;
+        }
+
+        .submit {
+            padding: 40px;
+            font-size: 2em;
+        }
+    }
+
+    @media screen and (max-width: ${config.responsive.width}px) {
+        form {
+            width: 100%;
+        }
+    }
 `;
 
 class NewNote extends React.Component {
+    constructor() {
+        super()
+        this.state = { title: '', content: '' }
+    }
+
+    handleTypingTitle(event) {
+        this.setState({ title: event.target.value })
+    }
+
+    handleTypingContent(event) {
+        this.setState({ content: event.target.value })
+    }
+
+    async submit() {
+        if (!this.state.title.length && !this.state.content.length)
+            return;
+        await client.mutate({
+            mutation: gql`
+                {
+                    addNote(title: ${this.state.title}, content: ${this.state.content}) {id}
+                }
+            `,
+        })
+    }
+
     render() {
         return (
             <Page_>
-                <div>write a new note</div>
+                <form>
+                    <h3>New note</h3>
+                    <input type="text" placeholder="Title" value={this.state.title} onChange={this.handleTypingTitle} />
+                    <textarea type="text" placeholder="Content" value={this.state.content} onChange={this.handleTypingContent} />
+                    <div className="submit" onClick={() => { this.submit() }}>Submit</div>
+                </form>
             </Page_>
         );
     }
